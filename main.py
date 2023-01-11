0e40ce602f2c105d26bb452dfc141b66be480545
Christian Dülsen <christian.duelsen@fielmann.com>
2023-01-11 11:52:59 +0100

    removed application id from code

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import classes.application_class as application_class 
+import json
 
-app = application_class.Application( id = "bf17b4a90124e900d3e0efffeedacabd" )
+application_infos = {}
+with open('application_env.json', 'r') as env_json:    
+    application_infos = json.load(env_json)
+
+
+app = application_class.Application( id = application_infos["application_id"] )
 app.run()
